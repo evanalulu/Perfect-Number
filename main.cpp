94b9d71ea23fbdbade1798ffc2c83f554c8590e5
Evana <77488474+evanalulu@users.noreply.github.com>
2022-09-25 23:31:50 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+bool isPerfect(int n) {
+    int sum = 0;
+    // Returns true if n is perfect, false otherwise.
+    for (int i = 1; i < n; i++) {
+        if ((n % i) == 0) {
+            sum += i;
+        }
+    } 
+    if (sum != n) {
+      return false;
+    }
+    return true;
+}
+
+int main() {
+  for (int i = 1; i < 10000; i++) {
+    if (isPerfect(i)) {
+      cout << i << " is perfect! \n";
+    }
+  }
+}
